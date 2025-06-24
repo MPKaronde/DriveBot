7b20def4d62cc685bd968814aa484e3abb876a43
MPKaronde <karonde.manav@gmail.com>
2025-06-24 11:48:22 -0500

    added method to drive straight & to convert cm to steps

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -43,6 +43,38 @@ int get_speed_percentage(double percent)
     return ((double)(MAX_SPEED - MIN_SPEED) * percent) + MIN_SPEED;
 }
 
+// Input: number cm desired to traverse (double w/ precison up to 4 decimals)
+// Output: number motor ticks required
+int get_steps_from_cm(double cm)
+{
+    return (int)(cm * (double)TICKS_PER_CM);
+}
+
+// Input: Speed (motor speed) & Distance (ticks) desired to drive in straight line
+// Behavior: Robot drives at given speed for given distance in straight line
+// Return: True if valid values, False if invalid values or error deteced
+bool drive_straight(int speed, int distance)
+{
+    // speed out of range
+    if (speed < MIN_SPEED || speed > MAX_SPEED)
+    {
+        return false;
+    }
+
+    // set desired speed and distance into motors
+    Left.setMaxSpeed(speed);
+    Right.setMaxSpeed(speed);
+    Left.moveTo(distance);
+    Right.moveTo(distance);
+
+    // run motors as needed
+    while (Left.distanceToGo() != 0 || Right.distanceToGo() != 0)
+    {
+        Left.run();
+        Right.run();
+    }
+}
+
 void loop()
 {
     int distance = 1000;
