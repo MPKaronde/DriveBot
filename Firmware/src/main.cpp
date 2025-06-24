a0779612b70b01613d0eaf136a5084763027013c
MPKaronde <karonde.manav@gmail.com>
2025-06-24 12:06:47 -0500

    added method to rotate in place by given number of degrees

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -9,6 +9,8 @@ const int MOTOR_ACCEL = 1000;     // motor acceleraton rate
 const int MAX_SPEED = 1000;       // (absolute value) max motor speed allowed
 const int MIN_SPEED = 400;        // (absolute value) min motor speed allowed
 const int TICKS_PER_CM = 35;      // Number of motor ticks per cm traveled in straight line NOTE: Actual calculated value = 35.0877193
+const int TRACK_WIDTH = 148;      // mm distance between ground contact of the drive wheels
+const int WHEEL_DIAMATER = 57;    // mm diameter of the wheels
 
 AccelStepper Right(HALFSTEP, 7, 6, 5, 4);
 AccelStepper Left(HALFSTEP, 8, 9, 10, 11);
@@ -73,6 +75,55 @@ bool drive_straight(int speed, int distance)
         Left.run();
         Right.run();
     }
+
+    return true;
+}
+
+// Input: Degrees to rotate by Speed (motor speed) to rotate in
+// Behavior: Rotate in place by given amount at given speed (runs both motors opposite directions for required time at given speed)
+// Return: True if valid values, False if invalid values or error deteced
+bool rotate_in_place(int degrees, int speed)
+{
+    // speed out of range
+    if (speed < MIN_SPEED || speed > MAX_SPEED)
+    {
+        return false;
+    }
+
+    // set motor speed
+    Left.setMaxSpeed(speed);
+    Right.setMaxSpeed(speed);
+
+    /*
+    Calculate Ticks::
+    Formulas:
+    Wheel Rotations = (TrackWidth * Degrees) / (360 * Wheel Diameter)
+    Ticks per rotation = 200
+    Therefore,
+    Ticks = (TrackWidth * Degrees) / (360 * 200 * Wheel Diameter)
+    */
+    int ticks = (TRACK_WIDTH * degrees) / (72000 * WHEEL_DIAMATER);
+
+    // Determine if going left or right
+    // TODO: determine if this is accurate
+    if (degrees > 0)
+    {
+        Left.moveTo(ticks);
+        Right.moveTo(-ticks);
+    }
+    else
+    {
+        Left.moveTo(-ticks);
+        Right.moveTo(ticks);
+    }
+
+    while (Left.distanceToGo() != 0 || Right.distanceToGo() != 0)
+    {
+        Left.run();
+        Right.run();
+    }
+
+    return true;
 }
 
 void loop()
